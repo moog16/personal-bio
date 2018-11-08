956aa83ad142ca1eb20da595b21a05ec5709db16
Matt Goo <mattgoo@google.com>
2018-11-07 23:03:52 -0800

    added headshot with animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Headshot from './Headshot';
+import TopAppBar, {TopAppBarFixedAdjust} from '@material/react-top-app-bar';
+import './App.scss';
 
 class App extends Component {
+  mainTopper = React.createRef();
+
+  state = {
+    headshotStyle: {},
+  };
+
+  calculateHeadshotTranslation = (_, isInitializing) => {
+    const {innerWidth, pageYOffset} = window;
+    const {offsetHeight} = this.mainTopper.current;
+    const margin = 30;
+    // when main__topper is at top of page - margin
+    const stopPoint = offsetHeight - pageYOffset - margin;
+    const multiple = pageYOffset/offsetHeight;
+    const maxMultiple = Math.min(multiple, 0.92);
+
+    if (stopPoint <= 0 && !isInitializing) return;
+    const translation = innerWidth/2 * maxMultiple;
+    this.setState({headshotStyle: {transform: `translate(${translation}px)`}});
+  }
+
+  componentDidMount() {
+    this.calculateHeadshotTranslation(null, true);
+    document.addEventListener('scroll', this.calculateHeadshotTranslation);
+  }
+
   render() {
+    const {headshotStyle} = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <TopAppBar fixed title='Matt Goo' actionItems={[<Headshot />]} />
+        <TopAppBarFixedAdjust tag='main' className='top-app-bar__fixed-adjust'>
+
+          <div className='content__topper' ref={this.mainTopper}></div>
+          <div className='content'>
+
+            <Headshot style={headshotStyle}/>
+
+          </div>
+        </TopAppBarFixedAdjust>
       </div>
     );
   }
